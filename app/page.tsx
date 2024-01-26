bce7dd44bd53a66f13637b5c113686abc1d109f1
NiBa97 <niba97@icloud.com>
2024-01-26 20:22:24 +0100

    feat: New components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,61 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
+import NodeCache from 'node-cache'
+import GitHub from 'github-api'
+import GithubRepositories from '@/components/GithubRepository'
 
-export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+const myCache = new NodeCache({ stdTTL: 24 * 60 * 60, checkperiod: 120 })
+
+// Define a type for repoData
+export type RepoData = {
+  title: string
+  stargazersCount: number
+  totalContributions: number
+  description: string
+  url: string
+}
+
+async function getGithubData() {
+  const repositories = ['vps-benchmarks', 'firefox-css-custom']
+  const owner = 'NiBa97'
+  const gh = new GitHub({
+    username: process.env.GITHUB_USERNAME,
+    token: process.env.GITHUB_TOKEN,
+  })
+  const data: Record<string, RepoData> = {}
+
+  for (const repository of repositories) {
+    let repoData: RepoData | undefined = myCache.get(`${owner}/${repository}`)
+
+    if (!repoData) {
+      console.log('Cache miss')
+      const repo = gh.getRepo(owner, repository)
+      const [details, contributors] = await Promise.all([repo.getDetails(), repo.getContributors()])
+
+      let contributions = 0
+      contributors.data.forEach((contributor) => {
+        contributions += contributor.contributions
+      })
+
+      repoData = {
+        title: repository,
+        stargazersCount: details.data.stargazers_count,
+        totalContributions: contributions,
+        description: details.data.description,
+        url: details.data.html_url,
+      }
+      // Save data to cache
+      myCache.set(`${owner}/${repository}`, repoData)
+    }
+
+    data[repository] = repoData
+  }
+
+  return { data }
+}
+export default async function Home() {
+  const data = await getGithubData()
+  return (
+    <div>
+      <GithubRepositories repo_data={data} />
+    </div>
+  )
 }
--- components/GithubRepository.tsx
+++ components/GithubRepository.tsx
@@ -1,6 +1,35 @@
 'use client'
-import { RepoData } from '../app/Main'
-
+import { RepoData } from '../app/page'
+import Link from './Link'
+import { FaGithub } from 'react-icons/fa'
+import { FaCodeCommit } from 'react-icons/fa6'
+import { FaStar } from 'react-icons/fa'
+import Card from './Card'
+const GithubRepository = ({ repo_data }: { repo_data: RepoData }) => {
+  return (
+    <Card>
+      <div className=" flex h-full flex-col">
+        <div className="flex flex-row items-center gap-1">
+          <FaGithub className="text-2xl" />
+          <h3 className="text-2xl font-bold">
+            <Link href={repo_data.url}>{repo_data.title}</Link>
+          </h3>
+        </div>
+        <p>{repo_data.description}</p>
+        <div className="mt-auto flex flex-row justify-between">
+          <div className="flex flex-row items-center gap-1">
+            <FaCodeCommit />
+            <p>{repo_data.totalContributions} commit</p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <FaStar />
+            <p>{repo_data.stargazersCount} stars</p>
+          </div>
+        </div>
+      </div>
+    </Card>
+  )
+}
 const GithubRepositories = ({
   repo_data: { data },
 }: {
@@ -9,16 +38,11 @@ const GithubRepositories = ({
   return (
     <div>
       <h1>Github Repositories</h1>
-      <ul>
-        {Object.entries(data).map(([key, { description, stargazersCount, totalContributions }]) => (
-          <li key={key}>
-            <p>{key}</p>
-            <p>{description}</p>
-            <p>{stargazersCount}</p>
-            <p>{totalContributions}</p>
-          </li>
+      <div className="grid grid-cols-2 gap-3">
+        {Object.entries(data).map(([key, repo_data]) => (
+          <GithubRepository key={key} repo_data={repo_data} />
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
