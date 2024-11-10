ad312c86c0be2067d9947f1a3955814fc039760a
NiBa97 <niba97@icloud.com>
2024-11-10 08:51:34 +0100

    refactor: Slim down project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,30 @@
-import Link from './Link'
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import siteMetadata from '@/data/siteMetadata'
-import SocialIcon from '@/components/social-icons'
-import PostCard from './PostCard'
+import Link from "./Link";
+import siteMetadata from "@/data/siteMetadata";
+import SocialIcon from "@/components/social-icons";
 
 export default function Footer() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
   return (
     <footer>
-      <div className="mx-auto hidden max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-        <h3>Recent posts</h3>
-      </div>
-      <div className="left-0  hidden  w-screen grid-cols-3 gap-3 px-3 sm:hidden">
-        <PostCard post={posts[0]} />
-        <PostCard post={posts[1]} />
-        <PostCard post={posts[2]} />
-      </div>
-      <div className=" mx-auto mt-3 flex w-full max-w-3xl flex-row justify-between xl:max-w-5xl">
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          {/* <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} /> */}
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} /> */}
+          {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} /> */}
+          {/* <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} /> */}
+          {/* <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
+        </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-        </div>{' '}
+        </div>{" "}
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
         </div>
       </div>
     </footer>
-  )
+  );
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Personal Blog',
   author: 'Niklas Bauer',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Niklas Bauer',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
