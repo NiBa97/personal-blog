3636a5fe5ed0df1aa8e2a1f772320518ab6e08e7
NiBa97 <niba97@icloud.com>
2025-06-21 07:51:12 +0000

    feat: Mermaid support

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,18 +1,25 @@
 import TOCInline from 'pliny/ui/TOCInline'
-import Pre from 'pliny/ui/Pre'
+// import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import { AudioComponent } from './Audio'
+import PreviewableCode from './PreviewableCode'
+import Mermaid from './Mermaid'
 
 export const components: MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
-  pre: Pre,
+  pre: (props) => <PreviewableCode defaultView={'code'} renderers={renderers} {...props} />,
   table: TableWrapper,
   AudioComponent: AudioComponent,
   BlogNewsletterForm,
+}
+
+
+const renderers = {
+  mermaid: Mermaid,
 }
